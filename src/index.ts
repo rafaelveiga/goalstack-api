e25ba9c2dfbd5c659b07680d3e545066216336b5
Rafael Veiga <dsg.rafael@gmail.com>
2022-08-19 17:29:59 -0300

    feat: add mongoose, init mongo db

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
-const express = require("express");
-const dotenv = require("dotenv");
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
+
+(async () => {
+  const connection = await mongoose.connect("mongodb://mongodb:27017");
+
+  console.log(connection);
+})();
 
 app.get("/", (req, res) => {
   res.send("Express + TypeScript Server");
